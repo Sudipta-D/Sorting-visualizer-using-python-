0e39ba261492bf797c6bfc4a3df4a44e09af2a1d
Sudipta-D <86481718+Sudipta-D@users.noreply.github.com>
2025-01-01 15:21:35 +0530

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,14 +1,13 @@
-def insertion_sort(InputList):
-    for i in range(1, len(InputList)):
-        j = i-1
-        nxt_element = InputList[i]
-# Compare the current element with next one
-		
-        while (InputList[j] > nxt_element) and (j >= 0):
-            InputList[j+1] = InputList[j]
-            j=j-1
-        InputList[j+1] = nxt_element
-# you can add list of any numbers
-list = [6,5,3,1,8,7,2,4]
-insertion_sort(list)
-print(list)
+def insertion_sort(input_list):
+    for i in range(1, len(input_list)):
+        j = i - 1
+        next_element = input_list[i]
+
+        while j >= 0 and input_list[j] > next_element:
+            input_list[j + 1] = input_list[j]
+            j -= 1
+        input_list[j + 1] = next_element
+
+numbers = [6, 5, 3, 1, 8, 7, 2, 4]
+insertion_sort(numbers)
+print(numbers)
