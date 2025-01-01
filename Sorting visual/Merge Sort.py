9d50c8fb330745671e9b659bb70268691c8c64a3
Sudipta-D <86481718+Sudipta-D@users.noreply.github.com>
2025-01-01 15:07:31 +0530

    Update Merge Sort.py

--- Sorting visual/Merge Sort.py
+++ Sorting visual/Merge Sort.py
@@ -1,34 +1,27 @@
 def merge_sort(unsorted_list):
     if len(unsorted_list) <= 1:
         return unsorted_list
-# Find the middle point and divide the unsorted list
+
     middle = len(unsorted_list) // 2
     left_list = unsorted_list[:middle]
     right_list = unsorted_list[middle:]
 
     left_list = merge_sort(left_list)
     right_list = merge_sort(right_list)
-    return list(merge(left_list, right_list))
 
-# Merge the sorted halves
+    return merge(left_list, right_list)
 
-def merge(left_half,right_half):
-
-    res = []
-    while len(left_half) != 0 and len(right_half) != 0:
+def merge(left_half, right_half):
+    result = []
+    while len(left_half) > 0 and len(right_half) > 0:
         if left_half[0] < right_half[0]:
-            res.append(left_half[0])
-            del left_half[0]
+            result.append(left_half.pop(0))
         else:
-            res.append(right_half[0])
-            del right_half[0]
-    if len(left_half) == 0:
-        res = res + right_half
-    else:
-        res = res + left_half
-    return res
-# You can add any list of numbers here
-unsorted_list = [6,5,3,1,8,7,2,4]
+            result.append(right_half.pop(0))
 
-print(merge_sort(unsorted_list))
+    result.extend(left_half if left_half else right_half)
+    return result
+
+numbers = [6, 5, 3, 1, 8, 7, 2, 4]
+print(merge_sort(numbers))
 
